c09a62238134217c1492d4c75ea995683476d2f1
BetomaxDJ <idsjorge@hotmail.com>
2022-01-25 18:26:18 -0600

    Correccion calcula edad

--- frontend/js/calculaEdad.js
+++ frontend/js/calculaEdad.js
@@ -3,7 +3,7 @@ window.calculaEdad = function (nacimiento){
 	console.log(nacimiento);
 	const fechaActual = new Date();
 	const anoActual = parseInt(fechaActual.getFullYear());
-	const mesActual = parseInt(fechaActual.getMonth());
+	const mesActual = parseInt(fechaActual.getMonth() + 1);
 	const diaActual = parseInt(fechaActual.getDate());
 	
 	const anoNacimiento = parseInt(String(nacimiento).substring(0,4));
@@ -11,6 +11,7 @@ window.calculaEdad = function (nacimiento){
 	const diaNacimiento = parseInt(String(nacimiento).substring(8,10)); 
 	
 	let edad = anoActual - anoNacimiento;
+	
 	if (mesActual < mesNacimiento){
 		edad--;
 	}else if (mesActual=== mesNacimiento ){
